46f6ab63ec2b8bddf4943e0508d978daca878708
mathefagbe <fagbemi65@gmail.com>
2025-07-04 18:21:50 +0100

    change update on deployment

--- beaverly_app/settings.py
+++ beaverly_app/settings.py
@@ -92,10 +92,14 @@ if DEBUG:
     DATABASES = {
             "default": {
                 "ENGINE": "django.db.backends.postgresql",
-                "NAME": "beaverly_db",
-                "USER": "postgres",
-                "PASSWORD": "test",
-                "HOST": "localhost",
+                # "NAME": "beaverly_db",
+                # "USER": "postgres",
+                # "PASSWORD": "test",
+                # "HOST": "localhost",
+                "NAME": config("POSTGRES_DB"),
+                "USER": config("POSTGRES_USER"),
+                "PASSWORD": config("POSTGRES_PASSWORD"),
+                "HOST": config("POSTGRES_HOST"),
                 "PORT": 5432,
             }
     }
